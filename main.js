29bad76ece2e1a696fd381348b80a41a852547bf
sohamsinghal1211 <80926575+sohamsinghal1211@users.noreply.github.com>
2022-03-31 18:48:48 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+noseX=0;
+noseY=0;
 function preload(){
+mus_mouth=loadImage('https://i.postimg.cc/xT8q9KYJ/download-mustache-free-png-transparent-image-and-clipart-75561-removebg-preview.png')
 }
 function setup(){
 canvas=createCanvas(300,300);
@@ -8,11 +11,12 @@ video.size(300,300);
 video.hide();
 
 poseNet=ml5.poseNet(video,modelLoaded);
-poseNet.on("pose",gotPoses);
+poseNet.on('pose',gotPoses);
 }
 function draw()
 {
 image(video,0,0,300,300);
+image(mus_mouth,noseX-37,noseY-10, 80, 40);
 }
 function take_snapshot(){
     save('FunnyNoseFilter.png');
@@ -26,6 +30,8 @@ function gotPoses(results)
     if(results.length>0)
     {
       console.log(results);
+      noseX=results[0].pose.nose.x;
+      noseY=results[0].pose.nose.y;
       console.log('nose x = '+results[0].pose.nose.x) ; 
       console.log('nose y = '+results[0].pose.nose.y) ;
     }
